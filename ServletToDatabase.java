2617ebaf42e8a0bf0418f1267f2457a9c68774d3
Ryan Suzuki <ryanjsuzuki@gmail.com>
2015-08-23 12:48:10 +0900

    updated Servlet

--- ServletToDatabase.java
+++ ServletToDatabase.java
@@ -2,60 +2,51 @@
 import java.io.*;
 import java.sql.*;
 import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 
-public class ServletToDatabase {
+public class ServletToDatabase extends HttpServlet {
 
+    public void doPost(HttpServletRequest request, HttpServletResponse resonse)
+                throws ServletException, IOException {
 
-    public static boolean submit(String status, String priority, String assignee,
-                                    String summary, String detailedDescription) {
-        boolean classStatus = false;
-        Connection connection = null;
-        PreparedStatement pst = null;
+                Connection connection = null;
+                PreparedStatement pst = null;
 
-        //the server is currently set locally on Ryan's computer, MUST change this for external access.
-        String url = "jdbc:mysql://localhost:3306/";
-        String databaseName = "mysql";
-        String driver = "com.mysql.jdbc.Driver";
-        String userName = "root";
-        String password = "yolo7156rty7";
+                //the server is currently set locally on Ryan's computer, MUST change this for external access.
+                String url = "jdbc:mysql://localhost:3306/";
+                String databaseName = "mysql";
+                String driver = "com.mysql.jdbc.Driver";
+                String userName = "root";
+                String password = "yolo7156rty7";
 
-        try {
-            Class.forName(driver);
-            connection = DriverManager.getConnection(url + databaseName, userName, password);
+                try {
+                    Class.forName(driver);
+                    connection = DriverManager.getConnection(url + databaseName, userName, password);
 
-            //the mysql insert statement
-            String query = "INSERT INTO defectlist (status, priority, assignee, summary, detailedDescription)"
-                            + " values (?, ?, ?, ?, ?)";
+                    //retrieves information from HTML Form submission.
+                    String status = request.getParameter("status");
+                    String priority = request.getParameter("priority");
+                    String assignee = request.getParameter("assignee");
+                    String summary = request.getParameter("summary");
+                    String description = request.getParameter("description");
 
-            pst = connection.prepareStatement(query);
-            pst.setString(1, status);
-            pst.setString(2, priority);
-            pst.setString(3, assignee);
-            pst.setString(4, summary);
-            pst.setString(5, detailedDescription);
+                    //the mysql insert statement
+                    String query = "INSERT INTO defectlist (status, priority, assignee, summary, description)"
+                                    + " values (?, ?, ?, ?, ?)";
 
-            pst.execute();
+                    pst = connection.prepareStatement(query);
+                    pst.setString(1, status);
+                    pst.setString(2, priority);
+                    pst.setString(3, assignee);
+                    pst.setString(4, summary);
+                    pst.setString(5, description);
 
+                    pst.execute();
 
         } catch (Exception exception) {
             System.out.println("Sorry, but there was an error: " + exception);
-        } finally {
-              if (connection != null) {
-                  try {
-                      connection.close();
-                  } catch (SQLException e) {
-                      e.printStackTrace();
-                  }
-              }
-              if (pst != null) {
-                  try {
-                      pst.close();
-                  } catch (SQLException e) {
-                      e.printStackTrace();
-                  }
-              }
         }
-        return classStatus;
 } //end of submit()
 } //end of Class
