7b1f6688dfe6d7ee27fbf0c06b61581c166af8ba
zuk0 <ryanjsuzuki@gmail.com>
2015-08-24 01:22:48 +0900

    Create Database.java

---
+++ Database.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class Database {
+    public static Connection getConnection () {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection
+                      ("jdbc:mysql://localhost:3306/dbname", 
+                      "root", "password");
+                      return connection;
+        } catch (Exception e) {
+          System.out.println("An error occured: " + e);
+          return null;
+        }
+    }
+
+    public static void close(Connection connection) {
+      try {
+          connection.close();
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+    }
+} //end of Class
