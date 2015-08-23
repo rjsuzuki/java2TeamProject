e7905dfec7cd9e42ff90a3c8fa4e75fc184a0285
zuk0 <ryanjsuzuki@gmail.com>
2015-08-24 00:25:40 +0900

    Create DefectDao.java
    This file will hold the methods to add/edit the defect list.

---
+++ DefectDao.java
@@ -0,0 +1,53 @@
+
+import java.sql.*;
+import java.util.*;
+
+public class DefectDao {
+    private Connection connection;
+
+    public DefectDao() {
+      connection = Database.getConnection();
+    }
+
+    public void checkDefect(Defect defect) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT * from DFECTLIST where * =?");
+            ps.setString(1, defect.get);
+
+        }
+    }
+
+    public void addDefect(Defect defect) {
+          try {
+              PreparedStatement ps = connection.prepareStatement("INSERT INTO ");
+              ps.setString(1, defect.getStatus());
+              ps.setString(2, defect.getPriority());
+              ps.setString(3, defect.getAssignee());
+              ps.setString(4, defect.getSummary());
+              ps.setString(5, defect.getDescription());
+              ps.executeUpdate();
+          } catch (SQLException e) {
+              e.printStackTrace();
+          }
+    }
+
+    public void deleteDefect(int defectId) {
+        try {
+            PrepararedStatement ps = connection.prepareStatement("");
+            ps.setString(1, defectId);
+            ps.executeUpdate();
+          } catch (SQLException e) {
+              e.printStackTrace();
+          }
+    }
+
+    //Need to add view method to see all Defects
+    //getAllDefects();
+
+    //Need to add email method? Does that go in here?
+
+    //Need to add assign a defect request to a person
+
+
+
+} //end of Class
